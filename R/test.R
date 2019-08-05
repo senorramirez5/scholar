f8e9e7970402038054452cdac7bcbb4e137fd189
Cristian Ramirez <crramire@macadms-MacBook-Pro.local>
2019-08-05 18:15:38 +0200

    Updated citation per year

--- R/test.R
+++ R/test.R
@@ -1,7 +1,9 @@
+library(magrittr)
+
 get_citations_publications = function(cid)
 {
   auths = ""
-  url_template = "http://scholar.google.com/scholar?oi=bibs&hl=es&scisbd=2&cites=%s"
+  url_template = "http://scholar.google.com/scholar?oi=bibs&hl=es&scisbd=2&cites=%s&hl=en"
   url = sprintf(url_template, as.character(cid))
   
   #https://scholar.google.com/scholar?hl=es&as_sdt=0%2C5&sciodt=0%2C5&cites=4539944020768348881&scipsc=&as_ylo=2019&as_yhi=2019
@@ -21,10 +23,28 @@ get_citations_publications = function(cid)
 #' @param exact_text Exact text to search
 #' @param author Author from publication
 #' @param from Starting point of search
+#' @param until Ending point of search
 #'
 #' @return Publication dataframe
 #' @export
-search_publication = function(text, exact_text="", author="",from=0)
+search_publication = function(text, exact_text="", author="",from=0,until=10)
+{
+  pubs<-search_publication_one_page(text,exact_text,author,from)
+  from <- from+10
+  while (from < until) {
+    pub<-search_publication_one_page(text,exact_text,author,from)
+    if(!is.data.frame(pub)){
+      from<-until
+    }
+    else{
+      pubs %<>% dplyr::bind_rows()
+      from <- from+10
+    }
+  }
+  return(pubs)
+}
+
+search_publication_one_page = function(text,exact_text,author,from)
 {
   url_template = "http://scholar.google.com/scholar?as_q=%s&as_epq=%s&as_sauthors=%s&start=%s&hl=en"
   url = sprintf(url_template, encode(text), encode(exact_text),encode(author),from)
@@ -40,12 +60,19 @@ search_publication = function(text, exact_text="", author="",from=0)
     rvest::html_text() %>% stringr::str_replace("\\[.*\\] ","")
   
   info <- pubs %>% rvest::html_nodes(xpath="//div[@class='gs_a']") 
-  authid<- rvest::html_node(info,xpath="a") %>% rvest::html_attr("href") %>%
-    stringr::str_extract("user=[a-zA-Z0-9_\\-]*&") %>% stringr::str_sub(6,-2)
   
-  authors<-sapply(info,FUN=function(data){
+  authors<-lapply(info,FUN=function(data){
+    links<- rvest::html_nodes(data,xpath="a")
+    link_authors<- links %>% html_text()
+    ids<- links %>% rvest::html_attr("href") %>%
+      stringr::str_extract("user=[a-zA-Z0-9_\\-]*&") %>% stringr::str_sub(6,-2)
     new_data <- data %>% rvest::html_text() %>% stringr::str_split("- ") %>% unlist()
-    new_data[1] %>% stringr::str_sub(1,-2)
+    auths<-new_data[1] %>% stringr::str_sub(1,-2) %>% stringr::str_replace("…","") %>%
+      stringr::str_split(",") %>% unlist() %>% stringr::str_trim()
+    authids<-rep(NA,length(auths))
+    index<-match(link_authors,auths)
+    authids[index]=ids
+    data.frame(author=auths,id=authids)
   })
   
   journals<-sapply(info,FUN=function(data){
@@ -80,14 +107,13 @@ search_publication = function(text, exact_text="", author="",from=0)
   })
   
   ## Put it all together
-  publications <- data.frame(title=title,
-                     author=authors,
-                     journal=journals,
-                     authorid=authid,
-                     cites=cites,
-                     year=years,
-                     cid=cid,
-                     pubid=pubid)
+  publications <- tibble::tibble(title=title,
+                             author=authors,
+                             journal=journals,
+                             cites=cites,
+                             year=years,
+                             cid=cid,
+                             pubid=pubid)
   
   
   return(publications)
@@ -121,10 +147,36 @@ get_num_results = function(page)
 #' @export
 get_citation_year = function(cid,year)
 {
-  url_template = "http://scholar.google.com/scholar?cites=%s&as_ylo=%s&as_yhi=%s"
+  url_template = "http://scholar.google.com/scholar?cites=%s&as_ylo=%s&as_yhi=%s&hl=en"
   url = sprintf(url_template, as.character(cid),year,year)
   
   page <- get_scholar_resp(url) %>% read_html()
   
   return(get_num_results(page))
+}
+
+#' Get citations per year
+#' 
+#' Gets the number of citations of a publication per year.
+#'
+#' @param cid Citation id of the publication
+#' @param year Year of publication
+#'
+#' @return Dataframe with number of cites per year
+#' @export
+get_citations_per_year = function(cid,year)
+{
+  years<-year:lubridate::year(Sys.Date())
+  cites<-sapply(years,get_citation_year, cid=cid)
+  
+  i=1;
+  cite<-get_citation_year(cid,year-i)
+  while (cite>0) {
+    years <- c(year-i,years)
+    cites<-c(cite,cites)
+    i<-i+1
+    cite<-get_citation_year(cid,year-i)
+  }
+  
+  return(data.frame(year=years,cites=cites))
 }
